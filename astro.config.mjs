c20d2c5a33508174bdddbe2811b13e83e190f43a
juliushonnor <juliushonnor@gmail.com>
2023-07-05 00:15:00 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ const env = loadEnv('', process.cwd(), 'STORYBLOK')
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
   integrations: [
     storyblok({
       //accessToken: env.STORYBLOK_TOKEN,
